43a834914b52159d8840e3a4a1e8da92f2e65e72
Faheemmalla <faheem.yousuf2004@gmail.com>
2026-02-17 19:05:53 +0530

    Query added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Path
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 
 app = FastAPI()
@@ -23,9 +23,28 @@ def view():
 
     return data
 @app.get('/patient/{patient_id}')
-def view_patient(patient_id : str = Path(...,description = 'ID of the Patient in the DB') ):
+def view_patient(patient_id : str = Path(...,description = 'ID of the Patient in the DB', example = "P001") ):
     #loading all patients
     data = load_data()
     if patient_id in data:
         return data[patient_id]
-    return {'error':'patient not found in the DB'}
+    raise HTTPException(status_code=404,detail="Patient Not Found")
+@app.get('/sort')
+#teen dots ka matlab ye required hai , yaane sort by required hai
+#without teen dots yaane optional
+def sort_patients(sort_by : str = Query(...,description='Sort on the basis of height,weight and bmi'),
+                  order : str = Query('asc',description='Sort in order of asc or desc order')):
+    valid_feilds = ['height','weight','bmi']
+    if sort_by not in valid_feilds:
+        raise HTTPException(status_code=400,detail=f'Invalid Field select From {valid_feilds}')
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400,detail='Invalid order select between asc and desc')
+    
+    data = load_data()
+    sort_order = True if order=='desc' else False
+    #reverse=True desending mai sort krta hai and False Ascending mai krta hai
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by,0),reverse=sort_order) # ye samjho gpt se
+
+    return sorted_data
+
+
